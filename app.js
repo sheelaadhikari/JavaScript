a942628ddd210a7a0e6ea85b2f39f7ab6bcd2fb8
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-12-13 11:31:30 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
-function even() {
+var container=document.querySelector(".container");
+var value1=document.querySelector(".input");
+var add1=document.querySelector(".add");
 
-    var tagSelector = document.getElementsByTagName('p');
 
-    for (i = 0; i < tagSelector.length; i++) {
+class item{
+    constructor (name){
+        this.create(name);
 
-        if (i % 2 == 0) {
-            tagSelector[i].textContent = i;
-        }
+    }
+    create(name){
+        var l1=document.createElement('div');
+        l1.classList.add('item');
+        var input=document.createElement('input');
+        input.type="text";
+        input.disabled=true;
+        input.value=name;
+        input.classList.add('item_iput');
+        var remove=document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML="Remove";
+        remove.addEventListener('click',()=>this.remove(l1));
+        container.appendChild(l1);
+        l1.appendChild(input);
+        l1.appendChild(remove);
+
+
+    }
+    remove(l1){
+        container.removeChild(l1);
 
 
     }
 
-
 }
 
-
-function odd() {
-
-    var tagSelector = document.getElementsByTagName('p');
-
-    for (i = 0; i < tagSelector.length; i++) {
-
-        if (i % 2 !== 0) {
-            tagSelector[i].textContent = i;
-        }
-
+add1.addEventListener('click',check);
+window.addEventListener('keydown', (e)=>{
+    if(e.which==13){
+        check();
 
     }
+})
 
+function check(){
+    if(value1.value!=""){
+        new item(value1.value);
+        value1.value="";
+    }
 
 }
+
+function removeAll(){
+    document.querySelector('.container').innerHTML="";
+}
+
