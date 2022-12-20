96b9f09967f313b912d9f2f2e2700ab0a9e4e96a
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2022-12-20 21:31:57 +0545

    Add files via upload

---
+++ tuesday.js
@@ -0,0 +1,15 @@
+
+
+
+
+var p = document.querySelector('.quote');
+p.addEventListener('click', function(event){
+    i.className = 'hidden';
+})
+
+var i = document.querySelector('.imag');
+i.addEventListener('click', function(event){
+   
+    p.className = 'hidden';
+})
+
